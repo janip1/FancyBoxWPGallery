41d2c94819e6a049acfb6a8b67bb53000c7a1943
Jani Poklar <jani.poklar@web-help.me>
2018-02-15 18:13:39 +0100

    Plugin done

--- gallery.php
+++ gallery.php
@@ -9,17 +9,18 @@ License:      GPLv2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-ini_set('display_errors','On');
-date_default_timezone_set("Europe/London");
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE);
-
 function gallery_scripts() {
     wp_deregister_script('jquery');
-    wp_register_script( 'jquery', 'js/jquery.min.js', array(), null, false);
-    wp_register_script('fancybox', 'js/jquery.fancybox.min.js');
-
-    wp_register_style('fancybox', 'css/jquery.fancybox.min.css');
+    wp_register_script( 'jquery', 'https://code.jquery.com/jquery-3.2.1.min.js', array(), null, false);
     wp_enqueue_script('jquery');
+
+    wp_register_script('fancybox', plugins_url('/js/jquery.fancybox.min.js', __FILE__) );
+    wp_register_style('fancybox', plugins_url('/css/jquery.fancybox.min.css', __FILE__) );
+    wp_enqueue_script('fancybox');
+
+    wp_register_script('image', plugins_url('/js/image.js', __FILE__) );
+    wp_enqueue_script('image');
+    wp_enqueue_style('fancybox');
 }
 add_action( 'init', 'gallery_scripts' );
 ?>
---
+++ js/image.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('.gallery-icon > a').attr('data-fancybox', 'gallery');
+});
